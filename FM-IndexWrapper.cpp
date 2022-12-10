e9ae23784d517f2e292a38ad69ef4434eacb0c46
Nispeter <nispeter2@gmail.com>
2022-12-10 13:45:54 -0300

    Documentando...

--- FM-IndexWrapper.cpp
+++ FM-IndexWrapper.cpp
@@ -17,41 +17,25 @@ vector<int> FMIndexWrapper::doc_locate(const string &pattern, vector<int> posLis
     sort(pos.begin(), pos.end());
     vector<int> docIndex;
 
-
-
-
 	if(!pos.size())
     return docIndex;
 
-  // for (int i = 0; i < posList.size(); i++)
-  // {
-  //   cerr<<posList[i]<<endl;
-  // }
+	int ocurCount = 0;
+	int lowRange = 0;
+	int upRange = posList[0];
+	int posCount = 0;
   
+	for(int i = 0; i < posList.size(); i++){
+		while(pos[posCount] > lowRange and pos[posCount] <= upRange){
+		posCount++;
+		ocurCount++;
+		}
+		if(ocurCount != 0)docIndex.push_back(i+1);;
+		ocurCount = 0;
 
-  int ocurCount = 0;
-  int lowRange = 0;
-  int upRange = posList[0];
-  int posCount = 0;
-
-  // for (int i = 0; i < pos.size(); i++)
-  // {
-  //   cerr<<"pos:"<<pos[i]<<endl;
-  // }
-  
-  
-  for(int i = 0; i < posList.size(); i++){
-    while(pos[posCount] > lowRange and pos[posCount] <= upRange){
-      //cerr<<pos[posCount] <<"<"<<upRange<<endl;
-      posCount++;
-      ocurCount++;
-    }
-      if(ocurCount != 0)docIndex.push_back(i+1);;
-      ocurCount = 0;
-
-    lowRange = upRange;
-    if(i != posList.size()-1)upRange = posList[i+1];
-    else upRange += posList[i];
-  }
-    return docIndex;
+		lowRange = upRange;
+		if(i != posList.size()-1)upRange = posList[i+1];
+		else upRange += posList[i];
+	}
+	return docIndex;
 }
--- sa.cpp
+++ sa.cpp
@@ -13,21 +13,27 @@ int_vector<> seq;
 
 
 int get_bound(int l, int r, char *text, char* pat, int_vector<> &sa, int mode){
+  /*
+  Funcion recursiva que realiza busqueda binaria sobre el arreglo de 
+  sufijos, retorna el lowerBound y Upperbound del suffix array ordenado 
+  donde se encuentran los patrones 
+  */
   int n = strlen(text);
   int m = strlen(pat);
   long mid = (l+r)/2;
-  //cerr<<"size "<<l<<" "<<mid<<" "<<r<<endl;
+
   if (l > r )
     return -1;
   else if(l == r or mid == 0 or mid == n-1)
     return mid;
-  
+  //Modo 1 obtiene el limite inferior 
   if(mode){
     if(strncmp(text+sa[mid], pat , m) >= 0)
       return get_bound(l,mid-1,text,pat,sa,mode);
     else
       return get_bound(mid+1,r,text,pat,sa,mode);
   }
+  //Modo 2 obtiene el limite superior 
   else {
     if(strncmp(text+sa[mid], pat , m) > 0)
       return get_bound(l,mid-1,text,pat,sa,mode);
@@ -38,25 +44,21 @@ int get_bound(int l, int r, char *text, char* pat, int_vector<> &sa, int mode){
 }
 
 vector<int> sa_locate(char *text, char* pat, int_vector<> &sa){
+  //sa_locate retorna un vector de enteros que contiene 
+  //la posiciond e cada ocurrencia del patron en la concatenacion de textos 
   vector<int> pos;
   int n = strlen(text);
   int m = strlen(pat);
-  //n size of text
-  //m size of pat 
 
   int l = 0;
   int r = n-m-1;
   int lb = get_bound(l,r,text,pat,sa,1)+1;
   int ub = get_bound(l,r,text,pat,sa,0);
 
-  //cerr<<"lower_bound "<<lb<<" "<<text[sa[lb]]<<endl;
-  //cerr<<"upper_bound "<<ub<<" "<<text[sa[ub]]<<endl;
   for(int i = lb;  i < ub; i++){
     pos.push_back(sa[i]);
-    //cerr<<sa[i]<<" ";
-  }//cerr<<endl;
+  }
   sort(pos.begin(),pos.end());
-  //cerr<<"a"<<endl;
   return pos;
 }
 
@@ -78,41 +80,28 @@ int_vector<> saCalculate(string filename, string patron) {
   
   int_vector<> sa(1, 0, bits::hi(n)+1);
   sa.resize(n);
+  //funcion base de algorithm que calcula el arreglo de sufijos y lo almacena en sa
   algorithm::calculate_sa((const unsigned char*)seq.data(), n, sa);
 
   return sa;
 }
 
 void doc_locate(string filename, string patron, vector<int> posList, int_vector<> sa){
+  /*doc_locate obtiene el archivo del cual aparece una ocurrenica del patron*/
+  //sa_locate 
   char* pat = (char*) patron.c_str();
-  
   vector<int> pos = sa_locate((char*)seq.data(), pat, sa);
 
-  //long int textCount = 0;
-  //long int sizeCount = 0;
   if(!pos.size())
     return ;
-
-  // for (int i = 0; i < posList.size(); i++)
-  // {
-  //   cerr<<posList[i]<<endl;
-  // }
-  
 
   int ocurCount = 0;
   int lowRange = 0;
   int upRange = posList[0];
   int posCount = 0;
-
-  // for (int i = 0; i < pos.size(); i++)
-  // {
-  //   cerr<<"pos:"<<pos[i]<<endl;
-  // }
-  
   
   for(int i = 0; i < posList.size(); i++){
     while(pos[posCount] > lowRange and pos[posCount] <= upRange){
-      //cerr<<pos[posCount] <<"<"<<upRange<<endl;
       posCount++;
       ocurCount++;
     }
@@ -122,23 +111,7 @@ void doc_locate(string filename, string patron, vector<int> posList, int_vector<
     lowRange = upRange;
     if(i != posList.size()-1)upRange = posList[i+1];
     else upRange += posList[i];
-  }
-  
-
-  // for (int i = 0; i <= pos.size() and pos[i] < posList[posList.size()-1]; i++) {
-  //   while(pos[i]> (textCount == 0 ? posList[0] : posList[textCount-1])){
-  //     if(ocurCount != 1) cout<<"Ocurrencias de patron '"<<pat<<"' en documento "<<textCount<<" = "<<ocurCount<<endl;
-  //     ocurCount = 1;
-  //     textCount++;
-  //   }
-  //   if(i != pos.size() and pos[i] < posList[textCount] )
-  //       ocurCount++;
-  //   else {
-  //     cout<<"Ocurrencias de patron '"<<pat<<"' en documento "<<textCount<<" = "<<ocurCount<<endl;
-  //     ocurCount = 1;
-  //     textCount++;
-  //   }
-  // }       
+  }  
 }
 
 double promedio(const vector<double> &v) {
